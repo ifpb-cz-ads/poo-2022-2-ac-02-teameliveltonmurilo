64bc413ba2cef5e3a52e3f9f57a565267dd77df8
livelton <105663496+livelton@users.noreply.github.com>
2022-10-13 19:02:07 -0300

    Programa que calcula a área de um triângulo

---
+++ CalculoAreaTriangulo.java
@@ -0,0 +1,13 @@
+public class CalculoAreaTriangulo {
+    public static void main(String[] args){
+
+        //estou supondo que o retângulo será de base 10 e altura 4
+        int base = 10;
+        int altura = 4;
+
+        int area = base * altura;
+
+        System.out.println("Base: " + base + " Altura: " + altura);
+        System.out.print("Area total: " + area);
+    }
+}
