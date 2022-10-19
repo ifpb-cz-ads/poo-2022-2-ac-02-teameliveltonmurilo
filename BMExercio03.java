73bac2e02faf2fb9ec0f25974355cf467024f8ab
livelton <105663496+livelton@users.noreply.github.com>
2022-10-19 08:46:20 -0300

    Imprimindo valores

--- BMExercio03.java
+++ BMExercio03.java
@@ -5,10 +5,13 @@ public class BMExercio03 {
         System.out.println("O item 1 custa " + item1 + " e o item 2 custa " + item2);
 
         double custoTotal = item1 + item2;
+        System.out.println("Custo total: " + custoTotal);
         final double taxa = 0.0825;
         double taxaCalculada = custoTotal * taxa;
+        System.out.println("Taxa Calculada: " + taxaCalculada);
         item1 += taxaCalculada; item2 += taxaCalculada;
         double novoCusto = item1 + item2;
+        System.out.println("Novo custo: " + novoCusto);
         boolean muitoCaro = novoCusto>10;
         System.out.println("Valor de muitoCaro: " + muitoCaro);
     }
